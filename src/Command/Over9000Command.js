c15fc8fd8dc37f079b40a30fa593d5b071c5c45a
Aaron Scherer <aequasi@gmail.com>
2016-02-02 21:16:34 -0800

    Update Over9000Command.js

--- src/Command/Over9000Command.js
+++ src/Command/Over9000Command.js
@@ -4,15 +4,9 @@ const _               = require('lodash');
 class Over9000Command extends AbstractCommand {
     static get name() { return 'over9000'; }
 
-    static get description() { return "WHAT 9000?!"; }
-
-    static get help() {
-        return `Mention anything \`over 9000\` to get a response from Nappa.`
-    }
+    static get description() { return "Mention anything \`over 9000\` to get a response from Nappa."; }
 
     handle() {
-        this.brain = this.container.get('brain.redis');
-
         this.hears(/over 9000/i, () => {
             this.reply("WHAT?! NINE THOUSAND?!");
         });
